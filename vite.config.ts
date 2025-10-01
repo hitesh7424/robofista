dcc092e90df5a5db219da27f2729060e0a56aaca
hitesh <hitesh164722@gmail.com>
2025-10-01 17:49:30 +0530

    feat: Configure Vite server settings for improved accessibility and port management

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from any IP address
+    port: 5173, // Default port (you can change this if needed)
+    strictPort: false, // Allow fallback to other ports if 5173 is in use
+  }
 })
